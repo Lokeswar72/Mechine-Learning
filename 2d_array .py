37331b8a0ab5173415a101879659f6a9ea2071d6
loki-basireddy <169087064+Lokeswar72@users.noreply.github.com>
2025-01-26 12:37:56 +0530

    dataframe

---
+++ 2d_array .py
@@ -0,0 +1,10 @@
+# create a 2D array and print it without using pandas
+
+import pandas as pd
+ 
+data= {'name': ['John', 'Doe', 'Smith'],
+         'age': [28, 34, 29],
+         'location': ['New York', 'California', 'Texas']} 
+df = pd.DataFrame(data)
+print(df)
+
